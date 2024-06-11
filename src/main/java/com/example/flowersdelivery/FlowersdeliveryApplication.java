669e1bced13dba5e60711023ddb8fb94bb3e57c9
61uk3 <128884577+61uk3@users.noreply.github.com>
2024-06-11 11:32:07 +0300

    start

---
+++ src/main/java/com/example/flowersdelivery/FlowersdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowersdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowersdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowersdeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flowersdelivery/FlowersdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowersdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowersdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
